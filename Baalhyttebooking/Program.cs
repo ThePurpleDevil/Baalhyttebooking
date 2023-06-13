4a5c2593c2438fac261ef70726cdd9eee289057b
Dobbe <Dobbe@LAPTOP-JB1OKR6R>
2023-06-13 20:00:47 +0200

    Add project files.

---
+++ Baalhyttebooking/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Baalhyttebooking
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello, world!");
+        }
+    }
+}
