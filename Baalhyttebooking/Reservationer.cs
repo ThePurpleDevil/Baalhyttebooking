7b4b4f34b071b4a042258aab2a94771718b66acc
Dobbe <henrik.martin@hotmail.com>
2023-06-14 13:08:24 +0200

    Reservationer

--- Baalhyttebooking/Reservationer.cs
+++ Baalhyttebooking/Reservationer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,7 +9,16 @@ namespace Baalhyttebooking
 {
     class Reservationer
     {
-   
-        
+
+        int _ID_Resvervationer;/*Reservationens ID,som skal Være unik*/
+        public Reservationer(int ID)
+        {
+            _ID_Resvervationer = ID;
+        }
+        public int ID
+        {
+            get { return _ID_Resvervationer; }
+        }
+
     }
 }
